0f9ccceeed2d96de07fed0badda6184a34b7b408
Federico Canet <fedeea@gmail.com>
2020-12-16 10:18:17 -0300

    Ft render.ejs 02 (#7)
    * Module 4 finished:
    EJS template engine added.
    
    * Module 5 started:
    Added book.ejs view and book routes.
    
    * Module 5 started:
    Added book.ejs view and book routes.
    
    Co-authored-by: Federico Canet <fcanet@velocitypartners.net>

--- app.js
+++ app.js
@@ -5,11 +5,30 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8888;
 
+var nav =  [
+    {
+        Link: '/Books',
+        Text: 'Books'
+    },
+
+    {
+        Link: '/Authors',
+        Text: 'Authors'
+    }
+];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
-    res.send('Hello, this is Dog!');
+    res.render('index', {title: 'Hello from render',
+        nav:[{Link: '/Books', Text: 'Books'}, {Link: '/Authors', Text: 'Authors'}]});
 });
 
 app.listen(port, function(error) {
